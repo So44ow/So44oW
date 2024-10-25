bc81305902aa9b055b536efd1a1d1aab5c48593c
SorroW <yayarikch1ll@gmail.com>
2024-10-25 17:14:07 +0300

    1.1

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Yaroslav Popov";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
